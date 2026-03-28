3a379c65cfa0608577a4ae410c09d6352aa6363a
nicomancebo <95196620+nicomancebo@users.noreply.github.com>
2026-03-28 12:46:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 const express    = require('express');
-const app = express();
 const cors       = require('cors');
 const axios      = require('axios');
 const NodeCache  = require('node-cache');
 const rateLimit  = require('express-rate-limit');
-app.get('/ping', (req, res) => {
-  res.status(200).send('ok');
 });
 /* ─────────────────────────────────────────────────────────────
    CONFIG
